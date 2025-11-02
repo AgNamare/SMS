4aaefc4aaf5027dce040f676d4474d846d0abcf0
Namare <AG@AG.local>
2025-11-02 16:42:54 +0300

    New commit

--- apps/web/src/modules/admission/repositories/admission-repository.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/src/app/lib/prisma";
-import { BaseRepository } from "../../core/repositories/base-repository";
-import { Prisma } from "@prisma/client";
-
-export class AdmissionRepository extends BaseRepository<Prisma.AdmissionDelegate<any>>{
-    constructor() {
-        super(prisma.admission)
-    }
-}
